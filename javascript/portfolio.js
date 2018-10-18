fd8680708530bb68c20cdc35d5b60b7626dd280f
Mark <contact.mshin@gmail.com>
2018-10-18 11:39:25 -0600

    snowflakes

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+    $("#logo").on("click", function (){
+        window.location.reload(true);
+    })
 
+    $("#background").on("click", function(){
+        $(".snowflake").stop(stopAll,goToEnd);
+    })
 });
