b354ad2f5c10cbb72c3472ac1f2cf4294b23ae9b
Mark <contact.mshin@gmail.com>
2018-10-18 17:48:23 -0600

    almost done

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -2,8 +2,4 @@ $(document).ready(function(){
     $("#logo").on("click", function (){
         window.location.reload(true);
     })
-
-    $("#background").on("click", function(){
-        $(".snowflake").stop(stopAll,goToEnd);
-    })
 });
